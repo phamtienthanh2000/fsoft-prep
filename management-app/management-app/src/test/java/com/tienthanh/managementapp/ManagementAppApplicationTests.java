7673c3dd9dcad900b2c51a91fd58ab9885a4104c
phamtienthanh2000 <yen17032000@gmail.com>
2024-01-18 14:07:57 +0700

    init code
    init code

---
+++ management-app/management-app/src/test/java/com/tienthanh/managementapp/ManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienthanh.managementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
